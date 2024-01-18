b44dbece05f361eb0154d41eeb03c230efdaa93d
dboybkru <dboy@bk.ru>
2024-01-18 23:28:54 +0200

    8 испр

--- synergy/решенные задания/урок 8 задание 2.py
+++ synergy/решенные задания/урок 8 задание 2.py
@@ -1,7 +1,6 @@
 s1 = input('Введите строку (учитывается только 1000 символов): ')
 s1 = s1[0:1000]
 s2 = str()
-a = 0
 for b in s1:    
     if b != ' ':
         s2 += b
@@ -9,6 +8,5 @@ for b in s1:
         if s2 [len(s2)-1:len(s2)] == ' ':
             continue
         else:
-            s2 += b      
-            #a = 0
+            s2 += b
 print(s2)
