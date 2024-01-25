27c79d29b401db5f183b9eba801ec104e12b71bd
dboybkru <dboy@bk.ru>
2024-01-25 20:41:49 +0200

    15 последний

--- synergy/решенные задания/урок 15 задание 1.py
+++ synergy/решенные задания/урок 15 задание 1.py
@@ -1,18 +1,11 @@
-class Transport:
-   def __init__(self, name, max_speed, mileage):
-        self.name = name
-        self.max_speed = max_speed
-        self.mileage = mileage
-        
-class Autobus(Transport):
+class Transport(object):    
     def __init__(self, name, max_speed, mileage):
-        #super().__init__(name, max_speed, mileage) Можно использоваь, но в уроке данный приём не рассмотрен, позволяет наследовать методы старшего класса
         self.name = name
         self.max_speed = max_speed
         self.mileage = mileage
-    def __str__(self): #Метод для вывода информации об объекте класса (найден на просторах интернета)
-        return f'Название автомобиля: {self.name}. Скорость: {str(self.max_speed)}. Пробег: {str(self.mileage)}.'
-bus = Autobus('Renault Logan', 180 , 12)
-print(bus)
+    def display_info(self):
+        print(f'Название автомобиля: {self.name}. Скорость: {str(self.max_speed)}. Пробег: {str(self.mileage)}.')   
+autobus = Transport('Renault Logan', 180 , 12)
+autobus.display_info()
 
 
