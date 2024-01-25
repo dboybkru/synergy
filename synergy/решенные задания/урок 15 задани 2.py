682902e1315888fa15a6312a7b1d51ca318dbe10
dboybkru <dboy@bk.ru>
2024-01-25 20:00:07 +0200

    15-3

--- synergy/решенные задания/урок 15 задани 2.py
+++ synergy/решенные задания/урок 15 задани 2.py
@@ -2,14 +2,10 @@ class Transport:
    def __init__(self, name, max_speed, mileage):
         self.name = name
         self.max_speed = max_speed
-        self.mileage = mileage
-        
+        self.mileage = mileage    
 class Autobus(Transport):
     def __init__(self, name, max_speed, mileage, seating_capacity):
-        #super().__init__(name, max_speed, mileage) Можно использоваь, но в уроке данный приём не рассмотрен, позволяет наследовать методы старшего класса
-        self.name = name
-        self.max_speed = max_speed
-        self.mileage = mileage
+        super().__init__(name, max_speed, mileage) #позволяет наследовать методы старшего класса
         self.seating_capacity = seating_capacity
     def __str__(self): #Метод для вывода информации об объекте класса (найден на просторах интернета)
         return f'Название автомобиля: {self.name}. Скорость: {str(self.max_speed)}. Пробег: {str(self.mileage)}. Вместимость {str(self.seating_capacity)}'
