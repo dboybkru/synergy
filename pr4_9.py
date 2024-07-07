a122d54438b645d591832864529eeada3a9cdd16
dboybkru <48415715+dboybkru@users.noreply.github.com>
2024-07-07 20:21:46 +0200

    Update pr4_9.py

--- pr4_9.py
+++ pr4_9.py
@@ -1,19 +1,30 @@
-ratings = [('Old York', 3.3), ('New Age', 4.6), ('Old Gold', 3.3), ('General Foods', 4.8),
-           ('Belissimo', 4.5), ('CakeAndCoffee', 4.2), ('CakeOClock', 4.2), ('CakeTime', 4.1),
-           ('WokToWork', 4.9), ('WokAndRice', 4.9), ('Old Wine Cellar', 3.3), ('Nice Cakes', 3.9)]
-         
-print(ratings)
-# Отсортируйте список ratings по убыванию рейтинга. Для кафе
-# с одинаковым рейтингом отсортируйте кортежи по названию.
+# Сортируем список ratings по убыванию рейтинга и названию кафе
+ratings = [
+    ('Old York', 3.3),
+    ('New Age', 4.6),
+    ('Old Gold', 3.3),
+    ('General Foods', 4.8),
+    ('Belissimo', 4.5),
+    ('CakeAndCoffee', 4.2),
+    ('CakeOClock', 4.2),
+    ('CakeTime', 4.1),
+    ('WokToWork', 4.9),
+    ('WokAndRice', 4.9),
+    ('Old Wine Cellar', 3.3),
+    ('Nice Cakes', 3.9),
+]
 
-ratings.sort(key=lambda x: x[0])
-print(ratings)
+ratings.sort(key=lambda x: (-x[1], x[0]))  # сортировка по убыванию рейтинга и по названию кафе
 
-ratings.sort(key=lambda x: x[1], reverse = True)
-print(ratings)
-        
-# Сохраните данные с рейтингом в словарь cafes, где ключами являются
-# названия кафе, а значениями - их рейтинг.
+# Создаем словарь cafes, где ключами являются названия кафе, а
+# значениями - их рейтинг
 from collections import OrderedDict
 
 cafes = OrderedDict(ratings)
+
+# Выводим отсортированный список и словарь
+print("Отсортированный список:")
+print(ratings)
+
+print("\nСловарь:")
+print(cafes)
